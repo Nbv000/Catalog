062f99826a6ab81968f4bd9ce9b52269afc109a9
Nbv000 <124570605+Nbv000@users.noreply.github.com>
2023-02-18 18:27:38 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="/css/style.css">
+	<title>
+		Электронный каталог
+	</title>
+</head>
+<body>
+	<header>
+  		<div class="header-left"><p>Электронный каталог "Венок сонетов"</p></div>
+  		<div class="header-right"><p><div id="regAuth"><a class="regAuth" href="#">Регистрация | Авторизация</a></div></p></div>
+	</header>
+	<div><p class="Main">Зарегестрируйтесь или войдите под своими данными, чтобы продолжить работу</p></div>
+	<div class="enjoy"><p><blockquote>"Венок сонетов" - это электронный каталог, который содержит информацию о более чем тысяче произведений различных авторов, написанных в жанре сонета. Каталог создан для научных целей.</blockquote> <br><blockquote>Каждая запись содержит информацию об авторе, годе написания, названии произведения и его издании. Каталог является удобным инструментом для изучения и анализа сонетов, а также для поиска вдохновения и наслаждения поэзией.</blockquote> <br><blockquote>Приятной работы!</blockquote></p></div>
+	<footer>
+  <p class="footer">Данные актуальны на <?php echo date('Y')?> год</p>
+	</footer>
+</body>
+</html>
